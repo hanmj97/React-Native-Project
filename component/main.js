a018c29a55b01479cb63b3c072cd44035a3c508d
sk038 <stonekeeper038@naver.com>
2023-04-30 11:19:29 +0900

    commit version 0.1

--- component/main.js
+++ component/main.js
@@ -9,7 +9,7 @@ const Main = () => {
             <Text style={styles.member2}>test123123123123{"\n"}</Text>
 
             <Button onPress={() => Alert.alert('로그인 화면 이동')} title="Learn More" color="#666666"/>
-            {/* 알림창 라이브러리 찾아봐야할듯 */}
+            {/* 알림창 라이브러리 찾아봐야할듯 123 */}
         </View>
     );
 };
