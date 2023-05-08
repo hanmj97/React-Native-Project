8cd330edcbfd376ed5fd5a91ade186dbb5780717
hanmj97 <hanmj97@naver.com>
2023-05-08 20:33:03 +0900

    DB connect

--- component/main.js
+++ component/main.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button, Alert} from "react-native" ;
+import {StyleSheet, View, Text, Button, Alert} from "react-native";
+import Axios from "axios";
 
 const Main = () => {
+    const signup = () => {
+        Axios.post("http://115.23.176.107:8000/signup", {
+            id: "hanmj97",
+            pw: 7891,
+            name: "한민재",
+            mbti: "ISFJ",
+        }).then((res) => {
+            console.log("회원가입 버튼 클릭");
+        }).catch((e) => {
+            console.error(e);
+        });
+    }
+
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>react-native를 사용해서 어플 만들기</Text>
             <Text style={styles.member}>팀원 : 한민재, 박준석</Text>
             <Text style={styles.member2}>test123123123123{"\n"}</Text>
 
-            <Button onPress={() => Alert.alert('로그인 화면 이동')} title="Learn More" color="#666666"/>
+            <Button onPress={async () => {
+                signup();
+            }} title="Learn More" color="#666666"/>
+
             {/* 알림창 라이브러리 찾아봐야할듯 123 */}
         </View>
     );
